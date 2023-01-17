a07601ed7577dbccf9f56432f43be57632cb0997
Benson Ndiwa <bnkimtai@gmail.com>
2023-01-17 13:22:45 +0300

    Responsive

--- shopping_cart/src/Cart.js
+++ shopping_cart/src/Cart.js
@@ -1,8 +1,10 @@
+import { Link } from "react-router-dom";
+
 const Cart = () => {
     return (
-  <div class="container mx-auto mt-10">
-    <div class="flex shadow-md my-10">
-      <div class="w-3/4 bg-white px-10 py-10">
+  <div class="container mx-auto mt-5">
+    <div class="md:flex shadow-md my-10">
+      <div class="px-5 py-5 w-full md:w-3/4 bg-white md:px-10 md:py-10">
         <div class="flex justify-between border-b pb-8">
           <h1 class="font-semibold text-2xl">Shopping Cart</h1>
           <h2 class="font-semibold text-2xl">3 Items</h2>
@@ -16,7 +18,7 @@ const Cart = () => {
 
         {/* Item Area */}
 
-        <div class="flex items-center hover:bg-gray-100 -mx-8 px-6 py-5">
+        <div class="flex items-center hover:bg-gray-100 -mx-5 px-6 py-5">
           <div class="flex w-2/5"> 
             <div class="w-20">
               <img class="h-24" src="https://drive.google.com/uc?id=18KkAVkGFvaGNqPy2DIvTqmUH_nk39o3z" alt="" />
@@ -28,14 +30,14 @@ const Cart = () => {
             </div>
           </div>
           <div class="flex justify-center w-1/5">
-            <svg class="fill-current text-gray-600 w-3" viewBox="0 0 448 512"><path d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"/>
-            </svg>
+            {/* <svg class="fill-current text-gray-600 w-3" viewBox="0 0 448 512"><path d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"/>
+            </svg> */}
 
-            <input class="mx-2 border text-center w-8" type="text" value="1" />
-
+            <input class="mx-2 border text-center w-12" type="number" />
+{/* 
             <svg class="fill-current text-gray-600 w-3" viewBox="0 0 448 512">
               <path d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"/>
-            </svg>
+            </svg> */}
           </div>
           <span class="text-center w-1/5 font-semibold text-sm">$400.00</span>
           <span class="text-center w-1/5 font-semibold text-sm">$400.00</span>
@@ -46,11 +48,11 @@ const Cart = () => {
         <a href="#" class="flex font-semibold text-indigo-600 text-sm mt-10">
       
           <svg class="fill-current mr-2 text-indigo-600 w-4" viewBox="0 0 448 512"><path d="M134.059 296H436c6.627 0 12-5.373 12-12v-56c0-6.627-5.373-12-12-12H134.059v-46.059c0-21.382-25.851-32.09-40.971-16.971L7.029 239.029c-9.373 9.373-9.373 24.569 0 33.941l86.059 86.059c15.119 15.119 40.971 4.411 40.971-16.971V296z"/></svg>
-          Continue Shopping
+          <Link to="/products">Continue Shopping</Link>
         </a>
       </div>
 
-      <div id="summary" class="w-1/4 bg-slate-300 px-8 py-10">
+      <div id="summary" class="w-full md:w-1/4 bg-slate-300 px-8 py-10">
         <h1 class="font-semibold text-2xl border-b pb-8">Order Summary</h1>
         <div class="flex justify-between mt-10 mb-5">
           <span class="font-semibold text-sm uppercase">Items 3</span>
