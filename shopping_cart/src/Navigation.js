f231bcff13d14cbdfa0f3b570ef10953cd29fa68
Benson Ndiwa <bnkimtai@gmail.com>
2023-01-18 11:15:47 +0300

    Drop Down Done

--- shopping_cart/src/Navigation.js
+++ shopping_cart/src/Navigation.js
@@ -1,4 +1,6 @@
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
+
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
@@ -6,15 +8,35 @@ const Navigation = () => {
     const cart = useSelector(state => state.cart);
     const count = cart.cartItems.length;
 
+    let clicked = false;
+    const handleDropDown = () =>{
+        if(clicked){
+
+            document.querySelector('.dropDown').setAttribute("class","dropDown invisible md:invisible transition duration-700 ease-in-out bg-slate-300 ");
+            clicked=false;
+
+        }else if(!clicked){
+        
+            document.querySelector('.dropDown').setAttribute("class","dropDown visible md:invisible transition duration-700 ease-in-out bg-slate-300 ");
+
+            clicked = true;
+        }
+    }
+
     return ( 
-        <ul class="flex py-4 pr-8">
-            <div class="flex basis-1/4 items-center place-content-center">
-                <li class="">
+        <div>
+        <ul class="flex py-2 pr-8 border-b-2 md:border-b-0">
+            <div class="flex w-full md:basis-1/4 items-center">
+                <li class="flex basis-1/2 justify-start px-5">
                     <Link to="/">
                         <img class="align-middle" src={require('./images/samsung.png')} />
                     </Link>
                     
                 </li>
+                <div class="flex basis-1/2 place-content-end justify-end md:collapse">
+                    <MenuOutlinedIcon color='info' fontSize='large' onClick={ handleDropDown } />
+                </div>
+                
             </div>
             <div className="hidden md:flex basis-3/4 items-center place-content-end gap-8">
                 <li class="text-xl text-slate-50 hover:text-orange-400 align-text-top font-sans ">
@@ -41,6 +63,16 @@ const Navigation = () => {
 
             </div>
         </ul>
+        <div class='dropDown invisible md:invisible transition duration-700 ease-in-out bg-slate-300 '>
+            <ul class='flex justify-center items-center p-2'>
+                <li class='items-center border-black border-r-2 px-5'><Link to='/products'>Products</Link></li>
+                <li class='items-center border-black border-r-2 px-5'>Contacts</li>
+                <li class="px-5 text-xl text-slate-50 hover:text-orange-400 text-center font-sans ">
+                    <Link to="/cart"><ShoppingCartIcon color='primary' /><sup class="text-black rounded-full bg-orange-300 p-1 text- font-mono font-features sups">{ count }</sup></Link>
+                </li>
+            </ul>
+        </div>
+        </div>
      );
 }
  
