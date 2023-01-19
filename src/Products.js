e6ed81e069eeaf35d1282b9cdc7521e84e627805
Benson Ndiwa <bnkimtai@gmail.com>
2023-01-19 11:35:23 +0300

    proxy

--- src/Products.js
+++ src/Products.js
@@ -25,7 +25,6 @@ const Products = () => {
             return res.json();
         })
         .then((res)=>{
-            console.log(res);
             setLoading(false);
             setProducts(res);
         })
